540c5ccf89c55941530ea4016ff0192471ebc1a8
Kevin <kevincauchi15@gmail.com>
2024-07-22 23:17:26 -0400

    Added Authentication DB & Assembly AI API -

--- backend/ai_youtube_app/ai_youtube_app/settings.py
+++ backend/ai_youtube_app/ai_youtube_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +77,27 @@ WSGI_APPLICATION = 'ai_youtube_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+load_dotenv()
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': getenv('PGDATABASE'),
+    'USER': getenv('PGUSER'),
+    'PASSWORD': getenv('PGPASSWORD'),
+    'HOST': getenv('PGHOST'),
+    'PORT': getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
 }
 
 
@@ -122,3 +141,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_URL= 'login'
--- backend/ai_youtube_app/summary_generator/views.py
+++ backend/ai_youtube_app/summary_generator/views.py
@@ -1,5 +1,98 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.conf import settings
+import json
+from pytube import YouTube
+import os
+import assemblyai as aai
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def generate_summary(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            yt_link =  data['link']
+            return JsonResponse({'content': yt_link})
+        except (KeyError, json.JSONDecodeError):
+            return JsonResponse({'error': 'Invalid Data Sent'}, status= 400)
+        
+        #get tittle
+        title = yt_title(yt_link)
+        #get transcript
+
+        #use open ai to generate summary 
+        #save article to db
+        #retutn summary as response
+
+    else:
+        return JsonResponse({'error': 'Invalid Request Method'}, status= 405)
+    
+    def yt_title(link):
+        yt = YouTube(link)
+        title = yt.title
+        return title
+    
+    def download_audio(link):
+        yt = Youtube(link)
+        video = yt.streams.filter(only_audio=True).first()
+        out_file=video.download(output_path=settings.MEDIA_ROOT)
+        base, ext =os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        return new_file
+
+    def get_transcription(link):
+         audio_file = download_audio(link)
+         aai.settings.api_key = apii
+    
+
+
+def user_login (request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password =request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = "Invalid Username or Password"
+            return render(request, 'login.html', {'error_message':error_message})
+
+    return render(request, 'login.html')
+
+def user_signup (request):
+    if request.method=='POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password =request.POST['password']
+        repeatPassword=request.POST['repeatPassword']
+
+        if password == repeatPassword:
+            try:
+                user = User.objects.create_user(username,email,password)
+                user.save()
+                login(request, user)
+                return redirect ('/')
+            except:
+                error_message = 'Error Creating Account'
+                return render(request, 'signup.html' ,{'error_message':error_message})
+        else:
+            error_message = 'Password do not match.'
+            return render(request, 'signup.html' ,{'error_message':error_message})
+
+    return render(request, 'signup.html')
+
+def user_logout (request):
+    logout(request)
+    return redirect('/')
